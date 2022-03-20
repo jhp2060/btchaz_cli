7cfa6f0428b6db2af156a133d65bd1cbac3f6eba
jhp2060 <jhp2060@gmail.com>
2022-03-20 17:50:50 +0900

    refactor(components): changed styles of Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,27 @@
 import styled from 'styled-components';
 import React, { useState } from 'react';
 
-const Button = () => {
-  const [state, setState] = useState(200);
-  return <StyledButton>{state}</StyledButton>
+const Button = ({ text }) => {
+  // const [state, setState] = useState(200);
+  // return <StyledButton onClick={() => setState(state + 1)}>{text} {state}</StyledButton>
+  return (
+    <StyledButton>{text}</StyledButton>
+  );
 };
 
 const StyledButton = styled.button`
+  padding: 20px;
+  margin: 10px;
+  border: unset;
+  border-radius: 200px;
+  background: cornflowerblue;
+  color: cornsilk;
+  vector-effect: non-scaling-stroke;
 
+  :hover {
+    color: cornsilk;
+    background: mediumaquamarine;
+  }
 `;
 
 export default Button;
