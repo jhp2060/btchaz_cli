e3f588820b6a957c3625559b438d87164789605c
jhp2060 <jhp2060@gmail.com>
2022-03-20 17:52:38 +0900

    refactor(app): apply react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Button from './components/Button';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RPage from './pages/RPage';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className="App">
-      <Header></Header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>BTCHAZ!</h1>
-        <p>
-          예약 가능한 방탈출 테마 찾으러 가기
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button></Button>
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/r" element={<RPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
